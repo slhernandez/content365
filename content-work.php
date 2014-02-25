9c3c7ddeb7ca09043f79608c6f95e6ec09735a07
Steve Hernandez <steve.hernandez@demandmedia.com>
2014-02-25 09:08:56 -0600

    Updated the work page with Experian entry.

--- content-work.php
+++ content-work.php
@@ -1,3 +1,9 @@
 <div class="siteWrapper work">
   <h1>Our Customers</h1>
+  <div class="work-items">
+    <a href="http://contentsolutions.demandstudios.com/work/experian"><div class="work-item">
+      <img src="<?php bloginfo('template_directory'); ?>/images/work/experian/work_thumbnail_experian.png">
+      <p>Experian Consumer Services</p>
+    </div></a>
+  </div>
 </div>
